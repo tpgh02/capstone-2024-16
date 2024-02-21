a83e247d132422848e05f140b0c0fbc962c7f707
Hyeong Jun Noh <munhwas1140@naver.com>
2024-02-21 21:25:18 +0900

    [feat] 프로젝트 세팅

---
+++ backend/src/main/java/com/dodo/DodoApplication.java
@@ -0,0 +1,13 @@
+package com.dodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoApplication.class, args);
+	}
+
+}
