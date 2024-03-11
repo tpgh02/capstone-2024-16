d67a23bb34250f5df7a6439b18b0f901bddb7157
gyur2 <gyur2@kookmin.ac.kr>
2024-03-11 21:57:26 +0900

    [Feat] 메인(수정) 사진 버튼 추가
    방이름이랑 사진 받아서 넘김

---
+++ frontend/dodo/lib/components/m_todo.dart
@@ -0,0 +1,55 @@
+import 'package:dodo/components/todo.dart';
+import 'package:flutter/material.dart';
+
+class m_todo extends StatelessWidget {
+  int? room_num;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.white),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Text(
+                '오늘도 도전',
+                style: TextStyle(fontFamily: "kcc", fontSize: 20),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        margin: EdgeInsets.all(10),
+                        child: todo(
+                          "자취요리왕",
+                          Image.asset(
+                            "assets/images/cook.jpg",
+                            fit: BoxFit.cover,
+                          ),
+                        )),
+                    todo(
+                      "몰?루",
+                      Image.asset(
+                        "assets/images/cook.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+            ),
+          ],
+        ));
+  }
+}
---
+++ frontend/dodo/lib/components/todo.dart
@@ -0,0 +1,28 @@
+import 'package:dodo/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class todo extends StatelessWidget {
+  final String room_name;
+  final Image room_img;
+
+  const todo(this.room_name, this.room_img);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        width: 180,
+        height: 180,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.amber),
+        child: InkWell(
+          onTap: () {},
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: room_img,
+          ),
+        ),
+      ),
+    );
+  }
+}
