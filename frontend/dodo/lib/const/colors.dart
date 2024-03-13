df1351a51101efab07d20edff4190b30162486cd
gyur2 <gyur2@kookmin.ac.kr>
2024-03-13 14:14:03 +0900

    [Refactor] color 변경

--- frontend/dodo/lib/const/colors.dart
+++ frontend/dodo/lib/const/colors.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const PRIMARY_COLOR = Color(0xff1cb5e0);
 final LIGHTGREY = Color(0xffefefef);
 final DARKGREY = Color(0xff646464);
-final POINT_COLOR = Color(0xff000046);
+const POINT_COLOR = Color(0xff000046);
