2113270b5fa102ca7e9978a68e424719c452848c
gyur2 <gyur2@kookmin.ac.kr>
2024-03-15 03:54:53 +0900

    [Feat] dropdownbutton 수정
    dropdownbutton이 제 기능을 못해서 제대로 만듦

--- frontend/dodo/lib/screen/main2_screen.dart
+++ frontend/dodo/lib/screen/main2_screen.dart
@@ -1,34 +1,68 @@
+import 'package:dodo/components/m2_board.dart';
 import 'package:dodo/const/colors.dart';
 import 'package:flutter/material.dart';
 
-class main2Page extends StatelessWidget {
-  //const main2Page({super.key});
+class main2Page extends StatefulWidget {
+  const main2Page({super.key});
+
+  @override
+  State<main2Page> createState() => _main2PageState();
+}
+
+class _main2PageState extends State<main2Page> {
+  List _roomname = ['오.운.완', '지금 일어나지 않으면...', '자취요리왕'];
+  Object? _select = '오.운.완';
 
   @override
   Widget build(BuildContext context) {
-    final List _roomname = ['오.운.완', '지금 일어나지 않으면...', '자취요리왕'];
-    String _select = '오.운.완';
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: DropdownButton(
-          value: _select,
-          items: _roomname.map(
-            (value) {
-              return DropdownMenuItem(
-                value: value,
-                child: Text(value),
-              );
-            },
-          ).toList(),
-          onChanged: (value) {
-            //뭐가 문젠데 다 해줬잖아 setState()가 안뜸.
-            // setState(() {
-            //   dropDownValue = value!;
-            // });
-          },
-        ),
-      ),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Container(
+                margin: const EdgeInsets.all(15),
+                child: Row(
+                  children: [
+                    _roomBtn(),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    const Text(
+                      "에서 나는?",
+                      style: TextStyle(
+                          fontSize: 30, fontFamily: 'kcc', color: POINT_COLOR),
+                    ),
+                  ],
+                ),
+              ),
+              m2_board()
+            ],
+          )),
+    );
+  }
+
+  Widget _roomBtn() {
+    return DropdownButton(
+      value: _select,
+      items: _roomname.map(
+        (value) {
+          return DropdownMenuItem(
+            value: value,
+            child: Text(
+              value,
+              style: TextStyle(fontFamily: 'bm', fontSize: 20),
+            ),
+          );
+        },
+      ).toList(),
+      onChanged: (value) {
+        setState(() {
+          _select = value;
+        });
+      },
     );
   }
 }
