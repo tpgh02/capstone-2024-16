24d232f5c39af890700fcdb7b651f351a9a4ac21
Hyeong Jun Noh <munhwas1140@naver.com>
2024-03-14 16:03:05 +0900

    [Merge] 머지

--- backend/src/main/java/com/dodo/DodoApplication.java
+++ backend/src/main/java/com/dodo/DodoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @ConfigurationPropertiesScan("com.dodo.token")
