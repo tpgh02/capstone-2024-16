3dc00b2c73ca5d221ab89c5b1e29a0972fafdbd3
gyur2 <gyur2@kookmin.ac.kr>
2024-03-19 22:15:37 +0900

    [Feat] singup 서버 연결 완료

--- frontend/lib/screen/login_screen.dart
+++ frontend/lib/screen/login_screen.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 
 //이건 너가 만든 대로 넣으면 돼!
 class loginPage extends StatelessWidget {
-  const loginPage({super.key});
+  // final Map? userData;
+  final int? userId;
+  const loginPage({Key? key, this.userId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Row(
           children: [
             //수현이 파일들어갈 곳!
-            l_title('로그인')
+            l_title('로그인'),
+            Text("UID : $userId"),
           ],
         ),
       ),
--- frontend/lib/screen/signup_screen.dart
+++ frontend/lib/screen/signup_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:dodo/const/colors.dart';
-import 'package:dodo/screen/findpass_screen.dart';
+// import 'package:dodo/screen/findpass_screen.dart';
+import 'package:dodo/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,52 +13,30 @@ class SignupPage extends StatefulWidget {
   State<SignupPage> createState() => _SignupPageState();
 }
 
-Future<data> fetchInfo(Map<String, String> userData) async {
+Future<int> fetchInfo(Map<String, String> userData) async {
   var url = 'http://43.203.195.126:8080/api/v1/users/register';
+  final response = await http.post(
+    Uri.parse(url),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(userData),
+  );
   try {
-    final response = await http.post(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(userData),
-    );
-
     if (response.statusCode == 200) {
       print('회원가입 성공!');
-      return data.fromJson(json.decode(response.body));
+      Map<String, dynamic> responseData = json.decode(response.body);
+      int userId = responseData['userId'];
+      print('userid: $userId'); //log 찍는 걸로 차후에 변경하기
+      return userId;
     } else {
       print('회원가입 실패: ${response.body}');
       throw Exception('회원가입에 실패했습니다');
     }
   } catch (e) {
+    print(response.body);
     print('네트워크 오류: $e');
     throw Exception('네트워크 오류가 발생했습니다');
-  }
-}
-
-class data {
-  final String type;
-  final String email;
-  final String username;
-  final String password1;
-  final String password2;
-
-  data(
-      {required this.type,
-      required this.email,
-      required this.username,
-      required this.password1,
-      required this.password2});
-
-  factory data.fromJson(Map<dynamic, dynamic> json) {
-    return data(
-      type: 'password',
-      email: json["id"],
-      username: json["userName"],
-      password1: json["account"],
-      password2: json["balance"],
-    );
   }
 }
 
@@ -77,13 +56,6 @@ class _SignupPageState extends State<SignupPage> {
     TextEditingController _password1 = TextEditingController();
     TextEditingController _password2 = TextEditingController();
 
-    // var dev_data = jsonDecode(data);
-    // var type = dev_data[0];
-    // var email = dev_data[1];
-    // var username = dev_data[2];
-    // var password1 = dev_data[3];
-    // var password2 = dev_data[4];
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
@@ -100,157 +72,163 @@ class _SignupPageState extends State<SignupPage> {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 50),
-            child: Form(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  const Text(
-                    '회원가입',
-                    style: TextStyle(fontFamily: 'kcc', fontSize: 30),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  //이메일
-                  SizedBox(
-                    height: 50,
-                    child: TextField(
-                      controller: _email,
-                      keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(fontSize: 20),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          //borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
-                        ),
-                        labelText: '이메일 주소',
-                        filled: true,
-                        fillColor: Color(0xffEDEDED),
-                        labelStyle:
-                            TextStyle(color: Color(0xff4f4f4f), fontSize: 18),
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  //사용자이름
-                  SizedBox(
-                    height: 50,
-                    child: TextField(
-                      controller: _username,
-                      keyboardType: TextInputType.name,
-                      style: const TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          //borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
-                        ),
-                        labelText: '사용자이름',
-                        filled: true,
-                        fillColor: const Color(0xffEDEDED),
-                        labelStyle: TextStyle(color: DARKGREY, fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  //비밀번호
-                  SizedBox(
-                    height: 50,
-                    child: TextField(
-                      controller: _password1,
-                      keyboardType: TextInputType.visiblePassword,
-                      style: const TextStyle(fontSize: 20),
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          //borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
-                        ),
-                        labelText: '비밀번호',
-                        filled: true,
-                        fillColor: Color(0xffEDEDED),
-                        labelStyle:
-                            TextStyle(color: Color(0xff4f4f4f), fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  //비밀번호 확인
-                  SizedBox(
-                    height: 50,
-                    child: TextField(
-                      controller: _password2,
-                      keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(fontSize: 20),
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          //borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
-                        ),
-                        labelText: '비밀번호확인',
-                        filled: true,
-                        fillColor: Color(0xffEDEDED),
-                        labelStyle:
-                            TextStyle(color: Color(0xff4f4f4f), fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    style: style,
-                    onPressed: () {
-                      Map<String, String> userData = {
-                        'type': 'password',
-                        'email': _email.text,
-                        'username': _username.text,
-                        'password1': _password1.text,
-                        'password2': _password2.text,
-                      };
-                      fetchInfo(userData).then((data) {
-                        print("회원가입 완");
-                      });
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => findpassPage()));
-                    },
-                    child: const Text(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Form(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    const Text(
                       '회원가입',
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(fontFamily: 'kcc', fontSize: 30),
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //이메일
+                    SizedBox(
+                      height: 50,
+                      child: TextField(
+                        controller: _email,
+                        keyboardType: TextInputType.emailAddress,
+                        style: const TextStyle(fontSize: 20),
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            //borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          labelText: '이메일 주소',
+                          filled: true,
+                          fillColor: Color(0xffEDEDED),
+                          labelStyle:
+                              TextStyle(color: Color(0xff4f4f4f), fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //사용자이름
+                    SizedBox(
+                      height: 50,
+                      child: TextField(
+                        controller: _username,
+                        keyboardType: TextInputType.name,
+                        style: const TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            //borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          labelText: '사용자이름',
+                          filled: true,
+                          fillColor: const Color(0xffEDEDED),
+                          labelStyle: TextStyle(color: DARKGREY, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //비밀번호
+                    SizedBox(
+                      height: 50,
+                      child: TextField(
+                        controller: _password1,
+                        keyboardType: TextInputType.visiblePassword,
+                        style: const TextStyle(fontSize: 20),
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            //borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          labelText: '비밀번호',
+                          filled: true,
+                          fillColor: Color(0xffEDEDED),
+                          labelStyle:
+                              TextStyle(color: Color(0xff4f4f4f), fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    //비밀번호 확인
+                    SizedBox(
+                      height: 50,
+                      child: TextField(
+                        controller: _password2,
+                        keyboardType: TextInputType.emailAddress,
+                        style: const TextStyle(fontSize: 20),
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            borderSide: BorderSide.none,
+                          ),
+                          labelText: '비밀번호확인',
+                          filled: true,
+                          fillColor: Color(0xffEDEDED),
+                          labelStyle:
+                              TextStyle(color: Color(0xff4f4f4f), fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: style,
+                      onPressed: () {
+                        Map<String, String> userData = {
+                          'type': 'password',
+                          'email': _email.text,
+                          'username': _username.text,
+                          'password1': _password1.text,
+                          'password2': _password2.text,
+                        };
+                        fetchInfo(userData).then((data) {
+                          print("로그인으로 넘어감");
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => loginPage(userId: data),
+                            ),
+                          );
+                        }).catchError((error) {
+                          print("망할 에러$error");
+                          print("$userData");
+                        });
+                      },
+                      child: const Text(
+                        '회원가입',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            alignment: Alignment.bottomRight,
-            child: Image.asset('assets/images/turtle.png',
-                width: 150,
-                height: 150, //175
-                fit: BoxFit.cover,
-                alignment: Alignment.bottomCenter),
-          ),
-        ],
+            Container(
+              //margin: EdgeInsets.symmetric(horizontal: 20),
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/images/turtle_w_e.png',
+                  // width: 200,
+                  // height: 200, //175
+                  fit: BoxFit.cover,
+                  alignment: Alignment.bottomCenter),
+            ),
+          ],
+        ),
       ),
     );
   }
