3211787b825d40b7dd45c864df0be5671a1492a9
gyur2 <gyur2@kookmin.ac.kr>
2024-03-20 10:42:08 +0900

    [Feat] main2에 calendar 추가

--- frontend/lib/screen/main2_screen.dart
+++ frontend/lib/screen/main2_screen.dart
@@ -1,6 +1,7 @@
 import 'package:dodo/components/m2_board.dart';
 import 'package:dodo/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class main2Page extends StatefulWidget {
   const main2Page({super.key});
@@ -12,6 +13,11 @@ class main2Page extends StatefulWidget {
 class _main2PageState extends State<main2Page> {
   List _roomname = ['오.운.완', '지금 일어나지 않으면...', '자취요리왕'];
   Object? _select = '오.운.완';
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +44,29 @@ class _main2PageState extends State<main2Page> {
                   ],
                 ),
               ),
-              m2_board()
+              m2_board(),
+              TableCalendar(
+                onDaySelected: onDaySelected,
+                selectedDayPredicate: (day) {
+                  return isSameDay(selectedDate, day);
+                },
+                focusedDay: selectedDate,
+                firstDay: DateTime(2024),
+                lastDay: DateTime(2025),
+                headerStyle: HeaderStyle(
+                    titleCentered: true,
+                    formatButtonVisible: false,
+                    titleTextStyle: TextStyle(fontFamily: 'bm', fontSize: 20)),
+              ),
             ],
           )),
     );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
   }
 
   Widget _roomBtn() {
