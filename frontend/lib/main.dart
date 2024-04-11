47e9019f5911ce72470ac1d10afcb21881f4c5bf
gyur2 <gyur2@kookmin.ac.kr>
2024-04-11 12:59:27 +0900

    [Feat] 상점 화면 구성

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dodo/components/items.dart';
 import 'package:dodo/screen/findpass_screen.dart';
 import 'package:dodo/screen/gallery_screen.dart';
 import 'package:dodo/screen/login_screen.dart';
@@ -6,6 +7,7 @@ import 'package:dodo/screen/report_screen.dart';
 import 'package:dodo/screen/room_join.dart';
 import 'package:dodo/screen/signup_screen.dart';
 import 'package:dodo/components/certification.dart';
+import 'package:dodo/screen/store_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dodo/screen/main_screen.dart';
 
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: mainPage(), //Certification("hello"),
+      home: storePage(), //Certification("hello"),
     );
   }
 }
