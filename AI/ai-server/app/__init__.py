508c78a7d6fd7e1c34413b89a378283543d10e17
srcho01 <seorim5306@gmail.com>
2024-04-12 15:15:18 +0900

    [Refactor] Move main app to main.py

--- AI/ai-server/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from app.study import study_bp
-    app.register_blueprint(study_bp, url_prefix='/ai')
-
-    return app
