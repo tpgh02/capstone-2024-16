fe7b09d70c44d8888cbbede53f1666b61b3ff705
srcho01 <seorim5306@gmail.com>
2024-04-14 20:57:52 +0900

    [Feat] Add post-processing function and middleware
    post-processing function study_result : get OCR reulst in json and post the result file to backend server
    middleware : check if all requested hosts(To be changed when host of backend server is determined) are trusted. connect study_upload function and study_result function.

--- AI/ai-server/app/study.py
+++ AI/ai-server/app/study.py
@@ -1,13 +1,39 @@
 from fastapi import APIRouter
+from fastapi.middleware import Middleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+import requests
 
-from app.save_image import save_image
 from app.model.image import StudyImage
+from app.save_image import save_image
+from app.time_detection import time_detection
 
 
-study_router = APIRouter(prefix="/ai/study")
+study_router = APIRouter(prefix="/ai")
 
-@study_router.post("/upload")
-def study_upload(data: StudyImage):
+# post-processing function
+async def study_result(data: StudyImage, response):
+    # if status code is not 200, do not execute post-processing function
+    if response.status_code != 200:
+        return
+    
+    # get OCR result in json
+    json_result = await time_detection(data)
+    
+    # post result to Backend server
+    try:
+        requests.post('http://server.com/endpoint', json=json_result)
+    except requests.exceptions.HTTPError as e:
+        print("POST fail:", e)
+    
+# middleware  
+middleware = [
+    Middleware(TrustedHostMiddleware, allowed_hosts=["*"]), # for all hosts
+    Middleware(study_result) # post-processing function
+]
+
+# original function
+@study_router.post("/study", middleware=middleware) 
+async def study_upload(data: StudyImage):
     # Upload and save the image
-    response = save_image(data)
+    response = await save_image(data)
     return response
