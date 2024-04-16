3ff9875a987f1fbd8f96b7383b99759a66066242
srcho01 <seorim5306@gmail.com>
2024-04-16 23:47:55 +0900

    [Fix] Modify uvicorn host and delete ai server IP in config.json

--- AI/ai-server/run.py
+++ AI/ai-server/run.py
@@ -11,9 +11,4 @@ if __name__ == "__main__":
     if args.localhost:  # localhost
         uvicorn.run("app.main:app", host="127.0.0.1", port=8080)
     else:  # AWS host
-        # get host and port from config.json
-        import json
-        with open("config.json", "r") as f:
-            ai_ip = json.load(f)['ai']
-        
-        uvicorn.run("app.main:app", host=ai_ip['ip'], port=ai_ip['port'])
+        uvicorn.run("app.main:app", host="0.0.0.0", port=8080)
