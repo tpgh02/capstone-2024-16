cd7f36bf73e75b66b511c9472f65d3e5b05e6579
srcho01 <seorim5306@gmail.com>
2024-04-16 22:36:00 +0900

    [Perform] Add AWS host
    - Select localhost or AWS host using an argument --localhost or -l (default is AWS host)
    - Get IP address from config.json

--- AI/ai-server/run.py
+++ AI/ai-server/run.py
@@ -1,5 +1,19 @@
-from app.main import app
+# Argument
+import argparse
+parser = argparse.ArgumentParser(description="Choose host of AI server")
+parser.add_argument('--localhost', '-l', action='store_true', help='Option selected, it runs as localhost')
+args = parser.parse_args()
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
+    
+    if args.localhost:  # localhost
+        uvicorn.run("app.main:app", host="127.0.0.1", port=8080)
+    else:  # AWS host
+        # get host and port from config.json
+        import json
+        with open("config.json", "r") as f:
+            ai_ip = json.load(f)['ai']
+        
+        uvicorn.run("app.main:app", host=ai_ip['ip'], port=ai_ip['port'])
