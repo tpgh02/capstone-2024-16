6059145663f8435fb0770d2b7e8289a0a751cfa2
srcho01 <seorim5306@gmail.com>
2024-04-16 22:42:26 +0900

    [Fix] Change post-processing from Middleware to BackgroundTasks
    - Import the server address from config.json
    - Apply server address

--- AI/ai-server/app/study.py
+++ AI/ai-server/app/study.py
@@ -1,16 +1,19 @@
-from fastapi import APIRouter
-from fastapi.middleware import Middleware
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi import APIRouter, BackgroundTasks
 import requests
 
 from app.model.image import StudyImage
 from app.save_image import save_image
 from app.time_detection import time_detection
 
+# get server address
+import json
+with open("config.json", "r") as f:
+    server = json.load(f)['server']
+    server_addr = f"{server['ip']}:{server['port']}"
 
 study_router = APIRouter(prefix="/ai")
 
-# post-processing function
+# background function
 async def study_result(data: StudyImage, response):
     # if status code is not 200, do not execute post-processing function
     if response.status_code != 200:
@@ -21,19 +24,17 @@ async def study_result(data: StudyImage, response):
     
     # post result to Backend server
     try:
-        requests.post('http://server.com/endpoint', json=json_result)
+        requests.post(f'{server_addr}/api/v1/certification/ai-result', json=json_result)
     except requests.exceptions.HTTPError as e:
         print("POST fail:", e)
     
-# middleware  
-middleware = [
-    Middleware(TrustedHostMiddleware, allowed_hosts=["*"]), # for all hosts
-    Middleware(study_result) # post-processing function
-]
-
 # original function
-@study_router.post("/study", middleware=middleware) 
-async def study_upload(data: StudyImage):
+@study_router.post("/study")
+async def study_upload(data: StudyImage, background: BackgroundTasks):
     # Upload and save the image
     response = await save_image(data)
+    
+    # add background task
+    background.add_task(study_result, data, response)
+    
     return response
