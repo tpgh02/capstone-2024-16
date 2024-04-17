fb38555b41b0b8d81f650795f991fc4d35e1409d
Hyung Jun Noh <munhwas1140@naver.com>
2024-04-17 12:06:04 +0900

    [refactor] aiResponseData 리스트 Optional 로 바꿈

--- backend/src/main/java/com/dodo/certification/dto/AiResponseData.java
+++ backend/src/main/java/com/dodo/certification/dto/AiResponseData.java
@@ -1,13 +1,29 @@
 package com.dodo.certification.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Optional;
 
 @Data
 public class AiResponseData {
     private String code;
     private String message;
     private Long certificationId;
-    private List<String> result;
+    private Optional<List<String>> result;
+
+    @JsonCreator
+    public AiResponseData(
+            @JsonProperty("code") String code,
+            @JsonProperty("message") String message,
+            @JsonProperty("certification_id") Long certificationId,
+            @JsonProperty("result") Optional<List<String>> result
+    ) {
+        this.code = code;
+        this.message = message;
+        this.certificationId = certificationId;
+        this.result = result;
+    }
 }
