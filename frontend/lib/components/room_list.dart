e8dfb47e6bc281d5bebf0efb9c017d9b3506d6a7
SuHyeon Jeong <119032609+kmujsh@users.noreply.github.com>
2024-04-23 14:45:30 +0900

    [Fix] 내가 가입한 그룹 리스트 null 처리

--- frontend/lib/components/room_list.dart
+++ frontend/lib/components/room_list.dart
@@ -3,51 +3,32 @@ import 'package:dodo/components/room_main.dart';
 import 'package:flutter/material.dart';
 
 class room_list extends StatefulWidget {
-  final int num;
-  const room_list({super.key, required this.num});
+  final String room_title;
+  final int room_id;
+  final String room_type;
+  final String room_img;
+  final int room_mem;
+  final int room_maxmem;
+  final bool canChat;
+  final bool is_manager;
+  const room_list(
+      {super.key,
+      required this.room_title,
+      required this.room_id,
+      required this.room_type,
+      required this.room_img,
+      required this.room_mem,
+      required this.room_maxmem,
+      required this.canChat,
+      required this.is_manager});
 
   @override
   State<room_list> createState() => _roomListState();
 }
 
 class _roomListState extends State<room_list> {
-  final List<dynamic> postList = [
-    {
-      "room_title": "자취요리왕",
-      "room_id": 1,
-      "room_type": "default",
-      "room_img": "assets/images/cook.jpg",
-      "room_mem": 10,
-      "room_maxmem": 20,
-      "canChat": true,
-      "is_manager": true,
-    },
-    {
-      "room_title": "오운완",
-      "room_id": 2,
-      "room_type": "default",
-      "room_img": "assets/images/turtle_noradius.png",
-      "room_mem": 5,
-      "room_maxmem": 20,
-      "canChat": false,
-      "is_manager": false,
-    },
-    {
-      "room_title": "H.O.T",
-      "room_id": 3,
-      "room_type": "group",
-      "room_img": "assets/images/turtle_noradius.png",
-      "room_mem": 10,
-      "room_maxmem": 20,
-      "canChat": true,
-      "is_manager": true,
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
-    int num = widget.num;
-
     return Column(
       children: [
         Padding(
@@ -62,7 +43,7 @@ class _roomListState extends State<room_list> {
                   height: 100,
                   child: ClipOval(
                     child: Image.asset(
-                      "${postList[num]["room_img"]}",
+                      widget.room_img,
                       fit: BoxFit.cover,
                     ),
                   ),
@@ -74,23 +55,23 @@ class _roomListState extends State<room_list> {
                     onTap: () {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) {
-                        if (postList[num]["room_type"] == "default") {
+                        if (widget.room_type == "default") {
                           return room_main(
-                            room_title: postList[num]["room_title"],
-                            room_id: postList[num]["room_id"],
-                            room_mem: postList[num]["room_mem"],
-                            room_maxmem: postList[num]["room_maxmem"],
-                            canChat: postList[num]["canChat"],
-                            is_manager: postList[num]["is_manager"],
+                            room_title: widget.room_title,
+                            room_id: widget.room_id,
+                            room_mem: widget.room_mem,
+                            room_maxmem: widget.room_maxmem,
+                            canChat: widget.canChat,
+                            is_manager: widget.is_manager,
                           );
                         } else {
                           return room_group(
-                            room_title: postList[num]["room_title"],
-                            room_id: postList[num]["room_id"],
-                            room_mem: postList[num]["room_mem"],
-                            room_maxmem: postList[num]["room_maxmem"],
-                            canChat: postList[num]["canChat"],
-                            is_manager: postList[num]["is_manager"],
+                            room_title: widget.room_title,
+                            room_id: widget.room_id,
+                            room_mem: widget.room_mem,
+                            room_maxmem: widget.room_maxmem,
+                            canChat: widget.canChat,
+                            is_manager: widget.is_manager,
                           );
                         }
                       }));
@@ -100,7 +81,7 @@ class _roomListState extends State<room_list> {
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         Text(
-                          "${postList[num]["room_title"]}",
+                          widget.room_title,
                           style: const TextStyle(
                             fontFamily: "bm",
                             fontSize: 25,
@@ -109,8 +90,7 @@ class _roomListState extends State<room_list> {
                         Row(
                           children: [
                             const Icon(Icons.people),
-                            Text(
-                                ' ${postList[num]["room_mem"]}/${postList[num]["room_maxmem"]}'),
+                            Text(' ${widget.room_mem}/${widget.room_maxmem}'),
                           ],
                         ),
                       ],
