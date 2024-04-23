0a29c6ace315897175820a11f4ad1fcf0cefecf2
SuHyeon Jeong <119032609+kmujsh@users.noreply.github.com>
2024-04-23 16:26:18 +0900

    [Feat] 비밀방 표시 아이콘 추가

--- frontend/lib/components/room_list.dart
+++ frontend/lib/components/room_list.dart
@@ -6,22 +6,25 @@ import 'package:flutter/material.dart';
 class room_list extends StatefulWidget {
   final String room_title;
   final int room_id;
+  final String? room_pwd;
   final String room_type;
   final String room_img;
   final int room_mem;
   final int room_maxmem;
   final bool canChat;
   final bool is_manager;
-  const room_list(
-      {super.key,
-      required this.room_title,
-      required this.room_id,
-      required this.room_type,
-      required this.room_img,
-      required this.room_mem,
-      required this.room_maxmem,
-      required this.canChat,
-      required this.is_manager});
+  const room_list({
+    super.key,
+    required this.room_title,
+    required this.room_id,
+    this.room_pwd,
+    required this.room_type,
+    required this.room_img,
+    required this.room_mem,
+    required this.room_maxmem,
+    required this.canChat,
+    required this.is_manager,
+  });
 
   @override
   State<room_list> createState() => _roomListState();
@@ -63,33 +66,38 @@ class _roomListState extends State<room_list> {
                 Expanded(
                   child: GestureDetector(
                     onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        if (widget.room_type == "default") {
-                          return room_main(
-                            room_title: widget.room_title,
-                            room_id: widget.room_id,
-                            room_mem: widget.room_mem,
-                            room_maxmem: widget.room_maxmem,
-                            canChat: widget.canChat,
-                            is_manager: widget.is_manager,
-                          );
-                        } else {
-                          return room_group(
-                            room_title: widget.room_title,
-                            room_id: widget.room_id,
-                            room_mem: widget.room_mem,
-                            room_maxmem: widget.room_maxmem,
-                            canChat: widget.canChat,
-                            is_manager: widget.is_manager,
-                          );
-                        }
-                      }));
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            if (widget.room_type == "default") {
+                              return room_main(
+                                room_title: widget.room_title,
+                                room_id: widget.room_id,
+                                room_mem: widget.room_mem,
+                                room_maxmem: widget.room_maxmem,
+                                canChat: widget.canChat,
+                                is_manager: widget.is_manager,
+                              );
+                            } else {
+                              return room_group(
+                                room_title: widget.room_title,
+                                room_id: widget.room_id,
+                                room_mem: widget.room_mem,
+                                room_maxmem: widget.room_maxmem,
+                                canChat: widget.canChat,
+                                is_manager: widget.is_manager,
+                              );
+                            }
+                          },
+                        ),
+                      );
                     },
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
+                        // 방 이름
                         Text(
                           widget.room_title,
                           style: const TextStyle(
@@ -97,10 +105,25 @@ class _roomListState extends State<room_list> {
                             fontSize: 25,
                           ),
                         ),
+                        // 방 속성
                         Row(
                           children: [
+                            // 인원 수
                             const Icon(Icons.people),
                             Text(' ${widget.room_mem}/${widget.room_maxmem}'),
+                            // 비밀방 여부
+                            widget.room_pwd != null
+                                ? const Padding(
+                                    padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                                    child: Icon(
+                                      Icons.lock,
+                                      size: 18,
+                                    ),
+                                  )
+                                : const SizedBox(
+                                    width: 0.1,
+                                  ),
+                            // 인증방 타입
                           ],
                         ),
                       ],
