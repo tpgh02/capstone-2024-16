6c960725b88765e96dadc2f17091dc70e66f0e66
gyur2 <gyur2@kookmin.ac.kr>
2024-04-30 22:00:24 +0900

    [Perform] 사진 박스 수정
    이전 : 인증 사진 3개를 올려야하는 데 한번에 같은 사진이 뜸
    수정 후 : 각각 다른 사진 pick 가능

--- frontend/lib/components/certification.dart
+++ frontend/lib/components/certification.dart
@@ -14,200 +14,67 @@ class Certification extends StatefulWidget {
 }
 
 class _CertificationState extends State<Certification> {
-  GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
-  File? _image;
-  TextEditingController _emailEditingController = TextEditingController();
-  TextEditingController _nameEditingController = TextEditingController();
-  TextEditingController _passwordEditingController = TextEditingController();
-  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
-  bool _isEnabled = true;
-
-  Future<void> pickImage() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.image, // select only images
-    );
-    if (result != null) {
-      if (result.files.single.path != null) {
-        setState(() {
-          _isEnabled = true;
-          _image = File(result.files.single.path!);
-          print('File Path: ${_image!.path}');
-        });
-      } else {
-        print('File path is null');
-      }
-    } else {
-      // If the user cancels the selection
-      print("No image selected");
-    }
+  final ImagePicker _picker = ImagePicker();
+  final List<XFile?> _pickedImages = [];
+  void getImage(ImageSource source) async {
+    final XFile? image = await _picker.pickImage(source: source);
+    setState(() {
+      _pickedImages.add(image);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     const TextStyle style =
         TextStyle(color: Colors.white, fontFamily: "kcc", fontSize: 30);
-    int c_length = 3; //인증개수
+    int c_length = 1; //인증개수
     return Flexible(
-        child: c_length == 1
-            ? Container(
-                width: 180,
-                height: 180,
-                child: InkWell(
-                  onTap: () {
-                    pickImage(); //이미지 저장
-                  },
-                  child: Stack(
-                    children: [
-                      if (_image != null)
-                        Container(
-                          // width: 50,
-                          // height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: DARKGREY,
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.file(
-                              _image!,
-                            ),
-                          ),
-                        )
-                      else
-                        Container(
-                          alignment: Alignment.center,
-                          child: const Icon(
-                            Icons.camera_alt,
-                            color: PRIMARY_COLOR,
-                          ),
-                        ),
-                    ],
-                  ),
-                ),
-              )
-            : SingleChildScrollView(
-                physics: AlwaysScrollableScrollPhysics(),
-                child: Column(
-                  children: List.generate(c_length, (index) {
-                    return select();
-                  }),
-                ),
-              ));
+        child: SingleChildScrollView(
+      physics: AlwaysScrollableScrollPhysics(),
+      child: Column(
+        children: List.generate(c_length, (index) {
+          return select(index);
+        }),
+      ),
+    ));
   }
 
-  Container select() {
+  Widget select(int index) {
     return Container(
       width: 180,
       height: 180,
       margin: EdgeInsets.symmetric(vertical: 10), // 각 컨테이너 사이에 간격 추가
-      child: Flexible(
-        child: InkWell(
-          onTap: () {
-            pickImage(); // 이미지 저장
-          },
-          child: Stack(
-            children: [
-              if (_image != null)
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: DARKGREY,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.file(
-                      _image!,
-                    ),
-                  ),
-                )
-              else
-                Container(
-                  alignment: Alignment.center,
-                  child: const Icon(
-                    Icons.camera_alt,
-                    color: PRIMARY_COLOR,
+      child: InkWell(
+        onTap: () {
+          getImage(ImageSource.gallery);
+        },
+        child: Stack(
+          children: [
+            if (_pickedImages.length > index && _pickedImages[index] != null)
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: DARKGREY,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.file(
+                    File(_pickedImages[index]!.path), // XFile을 File로 변환
+                    fit: BoxFit.cover,
                   ),
                 ),
-            ],
-          ),
+              )
+            else
+              Container(
+                alignment: Alignment.center,
+                child: const Icon(
+                  Icons.camera_alt,
+                  color: PRIMARY_COLOR,
+                ),
+              ),
+          ],
         ),
       ),
     );
   }
-}
-
-//팝업 띄우는 함수
-void statedialog(context, state, File? _image) {
-  bool _isEnabled = _image != null;
-  showDialog(
-    context: context,
-    builder: (context) {
-      return Dialog(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "인증하기",
-              style: TextStyle(fontFamily: "kcc", fontSize: 20),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              color: Colors.red,
-              alignment: Alignment.center,
-              child: Stack(
-                children: [
-                  _image == null
-                      ? Container(
-                          // width: 50,
-                          // height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Image.asset(
-                            'assets/images/turtle_noradius.png',
-                          ),
-                        )
-                      : Container(
-                          // width: 50,
-                          // height: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            image: _image != null
-                                ? DecorationImage(
-                                    image: FileImage(_image),
-                                  )
-                                : null,
-                          ),
-                        ),
-                  Positioned(
-                    left: 80,
-                    bottom: -10,
-                    child: IconButton(
-                      onPressed: _isEnabled
-                          ? () async {
-                              // Do something
-                            }
-                          : null,
-                      icon: Icon(Icons.add_a_photo),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              icon: const Icon(Icons.close),
-            )
-          ],
-        ),
-      );
-    },
-  );
 }
