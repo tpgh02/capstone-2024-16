f863699a5538ae4461d871ada8b5da901f010480
SuHyeon Jeong <119032609+112jshjsh@users.noreply.github.com>
2024-05-19 00:48:42 +0900

    [Fix] Tag Overflow Fix

--- frontend/lib/components/s2_tag.dart
+++ frontend/lib/components/s2_tag.dart
@@ -12,28 +12,30 @@ class s2_tag extends StatefulWidget {
 class _s2_tag extends State<s2_tag> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: widget.tag!.map((hashtag) {
-        return Container(
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          //width: 100,
-          alignment: Alignment.centerLeft,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: Colors.white,
-              border: Border.all(color: DARKGREY, width: 1)),
-          child: Text(
-            "${hashtag}",
-            style: const TextStyle(
-              fontSize: 15,
-              fontFamily: "bm",
-              //color: Colors.black54,
-            ),
-          ),
-        );
-      }).toList(),
-    );
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: widget.tag!.map((hashtag) {
+            return Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              //width: 100,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.white,
+                  border: Border.all(color: DARKGREY, width: 1)),
+              child: Text(
+                "${hashtag}",
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontFamily: "bm",
+                  //color: Colors.black54,
+                ),
+              ),
+            );
+          }).toList(),
+        ));
   }
 }
