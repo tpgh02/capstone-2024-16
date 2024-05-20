0adbce58bd612350583fa24b817930e55bb8062d
tpgh02 <tpgh02@kookmin.ac.kr>
2024-05-20 19:23:30 +0900

    Revert "[refactor] 데이터 베이스 ddl-auto:none로 수정 및 스키마 추가."
    This reverts commit f5335590461a2f0039169be7a3ba7dab0b0b9375.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,4 @@
-insert into image(url) values ('https://my-dodo-bucket.s3.ap-northeast-2.amazonaws.com/image/default.png');
--- insert into room(can_chat, frequency, num_of_vote_success, end_day, image_id, max_user, now_user, category, certification_type, info, name, notice, password, periodicity, is_full, room_type) values (true, 1, 1, now(), 1, 1, 1, 'STUDY', 'ADMIN', 'hi', '테스트 공부방', 'hi','123', 'DAILY', true, 'AI');
--- insert into room(can_chat, frequency, num_of_vote_success, end_day, image_id, max_user, now_user, category, certification_type, info, name, notice, password, periodicity, is_full, room_type) values (true, 1, 1, now(), 1, 1, 1, 'STUDY', 'ADMIN', 'hi', '테스트 공부방(AI X) 비밀번호 X', 'hi','', 'DAILY', true, 'NORMAL');
--- insert into room_user(is_manager, created_time, room_id, user_id) values (true, NOW(), 1, 1);
--- insert into room_user(is_manager, created_time, room_id, user_id) values (true, NOW(), 2, 1);
+insert into room(can_chat, frequency, num_of_vote_success, end_day, image_id, max_user, now_user, category, certification_type, info, name, notice, password, periodicity, is_full, room_type) values (true, 1, 1, now(), 1, 1, 1, 'STUDY', 'ADMIN', 'hi', '테스트 공부방', 'hi','123', 'DAILY', true, 'AI');
+insert into room(can_chat, frequency, num_of_vote_success, end_day, image_id, max_user, now_user, category, certification_type, info, name, notice, password, periodicity, is_full, room_type) values (true, 1, 1, now(), 1, 1, 1, 'STUDY', 'ADMIN', 'hi', '테스트 공부방(AI X) 비밀번호 X', 'hi','', 'DAILY', true, 'NORMAL');
+insert into room_user(is_manager, created_time, room_id, user_id) values (true, NOW(), 1, 1);
+insert into room_user(is_manager, created_time, room_id, user_id) values (true, NOW(), 2, 1);
