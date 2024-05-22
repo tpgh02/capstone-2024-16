f30d9e9d067bb2bf408550e7f242fed214937644
gyur2 <gyur2@kookmin.ac.kr>
2024-05-22 21:33:36 +0900

    [Feat] 상점 마일리지 수신 추가

--- frontend/lib/screen/store_screen.dart
+++ frontend/lib/screen/store_screen.dart
@@ -1,9 +1,39 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:dodo/components/items.dart';
 import 'package:dodo/const/colors.dart';
 import 'package:dodo/const/server.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+
+Future<MyInfo> fetchMyInfo_GET() async {
+  final response =
+      await http.get(Uri.parse('$serverUrl/api/v1/users/me'), headers: {
+    'Authorization':
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjF9.8PJk4wE2HsDlgLmFA_4PU2Ckb7TWmXfG0Hfz2pRE9WU'
+  });
+
+  if (response.statusCode == 200) {
+    log('Mypage: Connected!: ${utf8.decode(response.bodyBytes)}');
+    return MyInfo.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+  } else {
+    throw Exception('Mypage: fail to connect');
+  }
+}
+
+class MyInfo {
+  final int mileage;
+
+  MyInfo({required this.mileage});
+
+  factory MyInfo.fromJson(dynamic json) {
+    return MyInfo(
+      mileage: json["mileage"],
+    );
+  }
+}
 
 Future<List<Store>> fetchStore() async {
   var headers = {
@@ -14,7 +44,7 @@ Future<List<Store>> fetchStore() async {
 
   final response = await http.get(
     Uri.parse(serverUrl + '/api/v1/creature/store'),
-    headers: headers, // 헤더를 요청과 함께 추가합니다
+    headers: headers,
   );
 
   if (response.statusCode == 200) {
@@ -22,9 +52,12 @@ Future<List<Store>> fetchStore() async {
         jsonDecode(utf8.decode(response.bodyBytes)); //한국어 깨지는 걸 방지하기 위함
     List<Store> stores =
         storeList.map((storeJson) => Store.fromJson(storeJson)).toList();
+    log("상점은 서버 오케이");
     return stores;
   } else {
-    throw Exception('Failed to load album');
+    log("상점이 문제여");
+    log(jsonDecode(utf8.decode(response.bodyBytes)));
+    throw Exception('Failed to load store data');
   }
 }
 
@@ -66,12 +99,14 @@ class storePage extends StatefulWidget {
 
 class _storePageState extends State<storePage> {
   final widgetkey = GlobalKey();
-  late Future<List<Store>>? futureStore;
+  late Future<List<Store>> futureStore;
+  late Future<MyInfo> _myInfo;
 
   @override
   void initState() {
     super.initState();
     futureStore = fetchStore();
+    _myInfo = fetchMyInfo_GET();
   }
 
   @override
@@ -100,18 +135,95 @@ class _storePageState extends State<storePage> {
                     style: TextStyle(
                         fontFamily: 'bm', fontSize: 30, color: POINT_COLOR),
                   ),
+                  Spacer(),
+                  FutureBuilder<MyInfo>(
+                    future: _myInfo,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.hasData) {
+                        final mileage = snapshot.data!.mileage;
+                        final displayMileage =
+                            mileage > 999 ? '999' : mileage.toString();
+
+                        return ElevatedButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                backgroundColor: LIGHTGREY,
+                                title: Text(
+                                  '잔여 포인트',
+                                  style: TextStyle(
+                                    fontFamily: "bm",
+                                  ),
+                                ),
+                                content: Text(
+                                  '$mileage',
+                                  style: TextStyle(
+                                    fontFamily: "bma",
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: Text(
+                                      '확인',
+                                      style: TextStyle(
+                                        fontFamily: "bm",
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                child: const Icon(
+                                  Icons.attach_money_rounded,
+                                  color: Colors.white,
+                                ),
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.amber,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                displayMileage,
+                                style: TextStyle(
+                                  color: POINT_COLOR,
+                                  fontFamily: 'bm',
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        return Text('No data');
+                      }
+                    },
+                  ),
                 ],
               ),
-
               Container(
-                height: 500,
+                height: 600,
                 child: FutureBuilder<List<Store>>(
                   future: futureStore,
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return CircularProgressIndicator();
                     } else if (snapshot.hasError) {
-                      //return Text('Error: ${snapshot.error}'); //디버깅시 사용
                       return Column(
                         children: [
                           SizedBox(
@@ -148,11 +260,9 @@ class _storePageState extends State<storePage> {
                         );
                       } else {
                         return Container(
-                          //color: Colors.yellow,
                           alignment: Alignment.centerRight,
                           padding: const EdgeInsets.all(20),
                           height: 195,
-
                           child: CustomScrollView(
                             slivers: <Widget>[
                               SliverGrid(
